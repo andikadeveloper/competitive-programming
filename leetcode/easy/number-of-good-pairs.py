b55229b102c07b69a5cb11bc5c7981bd4381cf8e
andikadeveloper <muhammadandika@refactory.id>
2024-06-09 18:43:08 +0700

    feat(1512): add number of good pairs

---
+++ leetcode/easy/number-of-good-pairs.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    count += 1
+                
+        return count
+    
+solution = Solution()
+print(solution.numIdenticalPairs([1,2,3,1,1,3]))
